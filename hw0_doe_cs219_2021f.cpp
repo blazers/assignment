5c8082ff08a324921502b4895984ccaf58f1cde2
Xinlian Liu <liu@hood.edu>
2021-08-18 18:02:31 -0400

    Add files via upload

---
+++ hw0_doe_cs219_2021f.cpp
@@ -0,0 +1,23 @@
+/*
+  Assignment 0
+  CS 219
+  Fall 2021
+  Dr. Liu
+
+  Author: John Doe
+  
+  This assignment outputs a string -- Hello World! 
+
+  To compile:
+  $ g++ -o hw0 hw0_doe_cs219_2021f.cpp
+  To run:
+  $ ./hw0
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!\n";
+}
